397215b73259e78bbb24fb5a997d24fa7e52ae46
Jihad Sayed <david.pales1@gmail.com>
2025-04-11 23:47:09 -0500

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import HomeSliderAPIView,HomeContainersAPIView
+from .views import HomeSliderAPIView,HomeContainersAPIView, github_webhook
 
 
 urlpatterns = [
     path('api/home/list/HomeSlider', HomeSliderAPIView.as_view()),
     
     path('api/home/list/HomeContainers', HomeContainersAPIView.as_view()),
-    path('github-webhook', HomeContainersAPIView.as_view()),
+    path("github-webhook/", github_webhook, name="github-webhook"),
 
 ]
