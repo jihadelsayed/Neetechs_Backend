7603e0497b6eedaf07f4708650510efe8edc1e8b
Jihad Sayed <david.pales1@gmail.com>
2025-04-11 21:13:12 -0500

    fix

--- Neetechs/asgi.py
+++ Neetechs/asgi.py
@@ -4,8 +4,9 @@ ASGI config for Neetechs project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
+
 import os
 
 import django
@@ -18,4 +19,3 @@ django.setup()
 application = get_default_application()
 
 #application = get_asgi_application()
-
