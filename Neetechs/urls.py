2f8095e8cf6d27e52842e4f0162ea8a442693276
Jihad Sayed <david.pales1@gmail.com>
2025-04-11 23:12:50 -0500

    Update urls.py

--- Neetechs/urls.py
+++ Neetechs/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 
-
+ 
     #path('devices', FCMDeviceAuthorizedViewSet.as_view({'post': 'create'}), name='create_fcm_device'),
 
     
